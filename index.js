d232d4865a41a56c04c2f2d1344e35fb9efd12f7
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-12-21 11:55:26 +0100

    formato

--- index.js
+++ index.js
@@ -17,18 +17,20 @@ function procesarUsuarios(usuarios) {
 
 // DESAFIO 2
 class PistolaDeJuguete {
-  // 🧠 El constructor recibe un solo argumento: el número máximo de proyectiles que acepta una pistola.
-  // 🧠 Las instancias creadas con esta clase se inician completamente cargadas y con el seguro puesto.
-  // 🧠 Las instancias tienen un método `seguro` que no recibe argumentos y cambia el estado del seguro.
-  //      * Al activar el seguro, el método retorna "¡Seguro puesto!"
-  //      * Al desactivar el seguro, retorna "¡Seguro quitado!"
-  // 🧠 Las instancias tienen un método `disparar` que no recibe argumentos:
-  //      * Si queda munición y el seguro está desactivado, el método gasta un proyectil y retorna "¡Bang!"
-  //      * Si se dispara sin munición o con el seguro puesto, retorna la cadena "¡Clic!"
-  // 🧠 Las instancias tienen un método `recargar` que recibe un número y recarga la instancia con ese número de proyectiles.
-  //      * Recargar devuelve una cadena en el siguiente formato: "¡Dispones de <número> proyectiles!"
-  //      * Si el número recibido como argumento excede la capacidad máxima, solo se recarga hasta la capacidad máxima.
-  //      * Si el método recibe un argumento que no sea un tipo número mayor que cero, devuelve la cadena "¡Error al recargar!"
-  // 🌟 PISTA: las instancias necesitan llevar un registro de su capacidad máxima así como de su capacidad actual.
+  /*
+  🧠 El constructor recibe un solo argumento: el número máximo de proyectiles que acepta una pistola.
+  🧠 Las instancias creadas con esta clase se inician completamente cargadas y con el seguro puesto.
+  🧠 Las instancias tienen un método `seguro` que no recibe argumentos y cambia el estado del seguro.
+       * Al activar el seguro, el método retorna "¡Seguro puesto!"
+       * Al desactivar el seguro, retorna "¡Seguro quitado!"
+  🧠 Las instancias tienen un método `disparar` que no recibe argumentos:
+       * Si queda munición y el seguro está desactivado, el método gasta un proyectil y retorna "¡Bang!"
+       * Si se dispara sin munición o con el seguro puesto, retorna la cadena "¡Clic!"
+  🧠 Las instancias tienen un método `recargar` que recibe un número y recarga la instancia con ese número de proyectiles.
+       * Recargar devuelve una cadena en el siguiente formato: "¡Dispones de <número> proyectiles!"
+       * Si el número recibido como argumento excede la capacidad máxima, solo se recarga hasta la capacidad máxima.
+       * Si el método recibe un argumento que no sea un tipo número mayor que cero, devuelve la cadena "¡Error al recargar!"
+  🌟 PISTA: las instancias necesitan llevar un registro de su capacidad máxima así como de su capacidad actual.
+  */
 
 }
